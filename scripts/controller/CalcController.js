06628716792643154b67fdbd715c76a12fbe84f0
GabrielSilvaMEL <99516302+GabrielSilvaMEL@users.noreply.github.com>
2022-11-06 15:07:33 -0300

    update

---
+++ scripts/controller/CalcController.js
@@ -0,0 +1,16 @@
+class CalcController {
+    //método é umafunção, porém dentro de uma classe
+    //atributo é uma variável mas dentro de uma classe.
+    //Uma classe é um conjunto de aatributos e métodos
+    //instancia é um objeto que representa uma classe 
+    
+    constructor(config) //métodos ou funções sempre precisam de parâmetros
+    {
+        this.displayCalc = "0"; //o this faz referência ao objeto instanciado  dessa classe
+            //this referencia atributos métodos
+        var dataAtual; //ISSO É UM ATRIBUTO, é similar a uma variável mas tem mais recursos
+
+    }
+
+
+}   
