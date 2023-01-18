96cdb8e891c4ebe7e841bc3649a597184b6ebbff
Gabriel Silva <gabrielsilvaalves3000@outlook.com>
2023-01-18 03:57:25 -0300

    aula 8 e parte da 9

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -4,12 +4,46 @@ class CalcController {
     //Uma classe é um conjunto de aatributos e métodos
     //instancia é um objeto que representa uma classe 
     
+    //DOM - Document object model
+    //BOM - Browser object model
     constructor(config) //métodos ou funções sempre precisam de parâmetros
     {
-        this.displayCalc = "0"; //o this faz referência ao objeto instanciado  dessa classe
+        this._displayCalc = "0"; //o this faz referência ao objeto instanciado  dessa classe
             //this referencia atributos métodos
-        var dataAtual; //ISSO É UM ATRIBUTO, é similar a uma variável mas tem mais recursos
+        this.currentDate; //ISSO É UM ATRIBUTO, é similar a uma variável mas tem mais recursos
+        this.initialize();
+        //encapsulamento protege quem acessa o que
+        // O underline a seguir:
+        //      this._displayCalc = "0";
+        //      aponta que o atributo é privado
+    }
 
+    //renderiza primeiro
+    //está dentro do objeto então posso referir com this
+    initialize(){
+
+
+    }
+
+    //GET e SET 
+    //Definem como acessar os valores
+
+    //displaycalc
+    get displayCalc(){
+        return this._displayCalc;
+
+    }
+    set displayCalc(valor){
+        this.displayCalc = valor;
+    }
+
+
+    //dataatual
+    get currentDate(){
+        return this.currentDate;
+    }
+    set currentDate(valor){
+        this._currentDate = valor;
     }
 
 }   
